c2539d6881033ca4383af6e47d3e93cce2ff26d1
Ryan Kosta <rkosta@ucsd.edu>
2023-11-01 08:08:57 -0700

    RecipeList: add tests

---
+++ test/PantryPal/RecipeListTest.java
@@ -0,0 +1,32 @@
+package PantryPal;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class RecipeListTest {
+	@Test
+	public void testRead() {
+		MockReadBehavior rb = new MockReadBehavior();
+
+		/* create and add recipes r1 then r2 */
+		Recipe r1 = new Recipe();
+		rb.recipes.add(r1);
+		Recipe r2 = new Recipe();
+		rb.recipes.add(r2);
+
+		/* create recipelist */
+		RecipeList rlist = new RecipeList(rb);
+
+		/* ensure reading is not done till explicitly requested */
+		assertEquals(0, rlist.getChildren().size());
+
+		rlist.read();
+
+		/* ensure top element is r1, and below that is r2 */
+		assertEquals(2, rlist.getChildren().size());
+		assertEquals(rlist.getChildren().get(0),r1);
+		assertEquals(rlist.getChildren().get(1),r2);
+	}
+}
