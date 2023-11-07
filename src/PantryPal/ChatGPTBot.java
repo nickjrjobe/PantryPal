5d766bf8689fb5524224ca2e5200f1fdfc543dc1
Bryce Hackel <brycemhackel@gmail.com>
2023-11-06 20:53:39 -0800

    update chatgpt file

--- src/PantryPal/ChatGPTBot.java
+++ src/PantryPal/ChatGPTBot.java
@@ -15,111 +15,95 @@ interface RecipeCreator {
     /**
      * Method for generating a recipe.
      *
-     * @param meal The type of meal for the recipe.
+     * @param meal        The type of meal for the recipe.
      * @param ingredients The list of ingredients for the recipe.
-     * @return A string response containing the generated recipe, or null if there is an error.
+     * @return A string response containing the generated recipe, or null if there
+     *         is an error.
      */
     public String makeRecipe(String meal, String ingredients);
 }
 
 /**
- * The ChatGPTBot class implements the RecipeCreator interface and provides a way to generate recipes
+ * The ChatGPTBot class implements the RecipeCreator interface and provides a
+ * way to generate recipes
  * using the OpenAI API.
  */
 public class ChatGPTBot implements RecipeCreator {
-    // Constants for API access
-    private static String API_ENDPOINT;
-    private static String API_KEY;
-    private static String MODEL;
-    public String ChatGPTBotOutput;  // Store the generated recipe
-    public String prompt;
+    private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
+    private static final String MODEL = "text-davinci-003";
+    private String apiKey;
+    public String chatGptBotOutput;
+    private static final String PROMPT = "Give me a %s recipe using any subset of the"
+            + "  following ingredients. It does not need to use all of them: %s. Please"
+            + " make the first line of your response the title of recipe, and the"
+            + " following lines the recipe itself Thank you.";
+    private static final int MAX_TOKENS = 500;
 
     public ChatGPTBot() {
-        API_ENDPOINT = "https://api.openai.com/v1/completions";
         ConfigReader configReader = new ConfigReader();
-        API_KEY = configReader.getOpenAiApiKey();
-        MODEL = "text-davinci-003";
-    }
-
-    /**
-     * Generates a prompt for ChatGPT based on the given meal and ingredients.
-     *
-     * @param meal The type of meal for the recipe.
-     * @param ingredients The list of ingredients for the recipe.
-     * @return A string containing the prompt
-     */
-    public String promptCreator(String meal, String ingredients) {
-        return "Give me a " + meal + " recipe using the following ingredients: " + ingredients + 
-        ". Please make the first line of the response the title of recipe. Thank you.";
+        apiKey = configReader.getOpenAiApiKey();
     }
 
     /**
      * Generates a recipe based on the given meal and ingredients.
      *
-     * @param meal The type of meal for the recipe.
+     * @param meal        The type of meal for the recipe.
      * @param ingredients The list of ingredients for the recipe.
-     * @return A string containing the generated recipe, or null if there is an error.
+     * @return A string containing the generated recipe, or null if there is an
+     *         error.
      */
     public String makeRecipe(String meal, String ingredients) {
         try {
-            // Create a prompt for generating a recipe based on the meal and ingredients
-            String prompt = promptCreator(meal, ingredients);
-            System.out.println(prompt);
+            String prompt = String.format(PROMPT, meal, ingredients);
 
-            // Maximum number of tokens for the response
-            int maxTokens = 100;
-
-            // Create a JSON request body
+            // Setup Request
             JSONObject requestBody = new JSONObject();
             requestBody.put("model", MODEL);
             requestBody.put("prompt", prompt);
-            requestBody.put("max_tokens", maxTokens);
+            requestBody.put("max_tokens", MAX_TOKENS);
             requestBody.put("temperature", 1.0);
 
-            // Create the HTTP Client
             HttpClient client = HttpClient.newHttpClient();
-
-            // Create the request object
             HttpRequest request = HttpRequest
-            .newBuilder()
-            .uri(new URI(API_ENDPOINT))
-            .header("Content-Type", "application/json")
-            .header("Authorization", String.format("Bearer %s", API_KEY))
-            .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
-            .build();
+                    .newBuilder()
+                    .uri(new URI(API_ENDPOINT))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", String.format("Bearer %s", apiKey))
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+                    .build();
 
-            // Send the request and receive the response
+            // Perform request
             HttpResponse<String> responseBody = client.send(
-            request,
-            HttpResponse.BodyHandlers.ofString()
-            );
+                    request,
+                    HttpResponse.BodyHandlers.ofString());
 
-            // Parse the JSON response
+            // Parse response
             JSONObject responseJson = new JSONObject(responseBody.body());
             JSONArray choices = responseJson.getJSONArray("choices");
             String generatedText = choices.getJSONObject(0).getString("text");
-            ChatGPTBotOutput = generatedText;
+            chatGptBotOutput = generatedText;
 
-        } catch(Exception e) {
-            // How can u possibly screw this up HmmHMHmmMMMmMHMmmmMmMMmmmm 🤔
-            return null;  // Return null in case of an error
+        } catch (Exception e) {
+            return null;
         }
 
-        return ChatGPTBotOutput;
+        return chatGptBotOutput;
     }
 }
 
 /**
- * The mockChatGPTBot class implements the RecipeCreator interface and provides a mock implementation
+ * The mockChatGPTBot class implements the RecipeCreator interface and provides
+ * a mock implementation
  * for testing purposes.
  */
 class mockChatGPTBot implements RecipeCreator {
     // Instance variables
     private String mockOutput = "";
+
     /**
      * A mock implementation of the makeRecipe method.
      *
-     * @param meal The type of meal for the mock recipe.
+     * @param meal        The type of meal for the mock recipe.
      * @param ingredients The list of ingredients for the mock recipe.
      * @return A string containing a mock recipe output.
      */
@@ -128,10 +112,10 @@ class mockChatGPTBot implements RecipeCreator {
     }
 
     /**
- * Sets the mockMeal and mockIngredients instance variables.
- *
- * @param mockOutput The string that will be returned when calling makeRecipe
- */
+     * Sets the mockMeal and mockIngredients instance variables.
+     *
+     * @param mockOutput The string that will be returned when calling makeRecipe
+     */
     public void setOutput(String mockOutput) {
         this.mockOutput = mockOutput;
     }
