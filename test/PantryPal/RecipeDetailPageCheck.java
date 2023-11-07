ef3a665f8f85fbd2a8f08a48944a83644c44a72a
Summit_ybo <ybo@ucsd.edu>
2023-11-06 16:26:56 -0800

    renamed testing file

--- test/PantryPal/RecipeDetailPageCheck.java
+++ test/PantryPal/RecipeDetailPageCheck.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RecipeDetailPageTest extends Application {
+public class RecipeDetailPageCheck extends Application {
 
 
     @Override
