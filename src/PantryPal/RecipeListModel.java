1a7bb7921c777ef8608ed1b0dd8ba2ad6ccb7ff6
Ryan Kosta <rkosta@ucsd.edu>
2023-11-12 10:03:53 -0800

    move recipe list model to own file

---
+++ src/PantryPal/RecipeListModel.java
@@ -0,0 +1,55 @@
+/* Code initially adapted from Lab1 */
+
+package PantryPal;
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javafx.event.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import org.json.JSONObject;
+
+/**
+ * Communication model for making API requests to get Recipe List.
+ */
+public class RecipeListModel {
+  public List<String> performRequest() {
+    try {
+      String urlString = "http://localhost:8100/recipes";
+      URL url = new URI(urlString).toURL();
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setDoOutput(true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String response = in.readLine();
+      System.out.println("response " + response);
+      in.close();
+      return processResponse(response);
+    } catch (Exception e) {
+      System.err.println("HTTP request failed with error " + e.getMessage());
+      return null;
+    }
+  }
+
+  /** Synchronize recipe List UI element with application's internal recipe list */
+  public List<String> processResponse(String response) throws IllegalArgumentException {
+    try {
+      JSONObject json = new JSONObject(response);
+      List<String> title = new ArrayList<String>();
+      Iterator<String> ititles = json.keys();
+      while (ititles.hasNext()) {
+        title.add(ititles.next());
+      }
+      return title;
+    } catch (Exception ex) {
+      throw new IllegalArgumentException("response: \"" + response + "\" was invalid");
+    }
+  }
+}
--- src/PantryPal/RecipeListPage.java
+++ src/PantryPal/RecipeListPage.java
@@ -16,41 +16,6 @@ import javafx.scene.layout.*;
 import javafx.scene.text.*;
 import org.json.JSONObject;
 
-class RecipeListModel {
-  public List<String> performRequest() {
-    try {
-      String urlString = "http://localhost:8100/recipes";
-      URL url = new URI(urlString).toURL();
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setDoOutput(true);
-      BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      String response = in.readLine();
-      System.out.println("response " + response);
-      in.close();
-      return processResponse(response);
-    } catch (Exception e) {
-      System.err.println("HTTP request failed with error " + e.getMessage());
-      return null;
-    }
-  }
-
-  /** Synchronize recipe List UI element with application's internal recipe list */
-  public List<String> processResponse(String response) throws IllegalArgumentException {
-    try {
-      JSONObject json = new JSONObject(response);
-      List<String> title = new ArrayList<String>();
-      Iterator<String> ititles = json.keys();
-      while (ititles.hasNext()) {
-        title.add(ititles.next());
-      }
-      return title;
-    } catch (Exception ex) {
-      throw new IllegalArgumentException("response: \"" + response + "\" was invalid");
-    }
-  }
-}
-
 /** UI element which displays list of recipes */
 class RecipeListUI extends VBox {
   RecipeListUI(List<RecipeEntryUI> entries) {
