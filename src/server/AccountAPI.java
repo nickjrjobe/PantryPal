c9d1b74f32bbd4b12ec5d683382c057b4a2af0e7
Ryan Kosta <rkosta@ucsd.edu>
2023-11-24 13:58:47 -0800

    Accounts: implement authorization api

---
+++ src/server/AccountAPI.java
@@ -0,0 +1,37 @@
+package server;
+
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.json.JSONObject;
+import utils.Account;
+
+class AuthorizationAPI extends HttpAPI {
+  private AccountData data;
+
+  AuthorizationAPI(AccountData map) {
+    this.data = map;
+  }
+
+  /** Authorize account */
+  String handlePost(String query, String request) throws IOException {
+    /* interpret request as json */
+    JSONObject json = getJSONRequest(request);
+    Account account;
+    Account existingAccount;
+    try {
+      account = new Account(json);
+    } catch (Exception e) {
+      throw new IOException(e.getMessage());
+    }
+    existingAccount = data.get(account.getUsername());
+    if (existingAccount == null) {
+      return "404 Not Found";
+    } else if (!existingAccount.equals(account)) {
+      return "401 Unauthorized";
+    } else {
+      return "200 Ok";
+    }
+  }
+}
