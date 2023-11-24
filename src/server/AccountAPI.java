248f2771f280d9ae01e93aa3b07a8d3c9e6baee1
Ryan Kosta <rkosta@ucsd.edu>
2023-11-24 14:08:59 -0800

    add Account API

--- src/server/AccountAPI.java
+++ src/server/AccountAPI.java
@@ -7,31 +7,46 @@ import java.util.*;
 import org.json.JSONObject;
 import utils.Account;
 
-class AuthorizationAPI extends HttpAPI {
+class AccountAPI extends HttpAPI {
   private AccountData data;
 
-  AuthorizationAPI(AccountData map) {
+  DetailedAccountAPI(AccountData map) {
     this.data = map;
   }
 
-  /** Authorize account */
+  /** Create a new account */
   String handlePost(String query, String request) throws IOException {
     /* interpret request as json */
+    String response = "406 Not Acceptable";
     JSONObject json = getJSONRequest(request);
     Account account;
-    Account existingAccount;
     try {
       account = new Account(json);
     } catch (Exception e) {
       throw new IOException(e.getMessage());
     }
-    existingAccount = data.get(account.getUsername());
-    if (existingAccount == null) {
-      return "404 Not Found";
-    } else if (!existingAccount.equals(account)) {
-      return "401 Unauthorized";
-    } else {
-      return "200 Ok";
+    if (data.get(account.getUsername()) == null) {
+      data.put(account.getUsername(), account);
+      response = "200 Ok";
     }
+    return response;
+  }
+
+  /** Update account */
+  String handlePut(String query, String request) throws IOException {
+    return handlePost(query, request);
+  }
+
+  /** Delete account */
+  String handleDelete(String query, String request) throws IOException {
+    String response = "404 Not Found";
+    if (query != null) {
+      String value = query.substring(query.indexOf("=") + 1);
+      Account account = data.remove(value); // Retrieve data from hashmap
+      if (account != null) {
+        response = "200 OK";
+      }
+    }
+    return response;
   }
 }
