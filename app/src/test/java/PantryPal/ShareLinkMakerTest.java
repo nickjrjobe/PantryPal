2e06c8425ab5275aa9187ba742bb8ef5f2d5679b
Ryan Kosta <rkosta@ucsd.edu>
2023-12-05 21:00:48 -0800

    fix sharelinkmakertest

--- app/src/test/java/PantryPal/ShareLinkMakerTest.java
+++ app/src/test/java/PantryPal/ShareLinkMakerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import utils.Account;
+import utils.ConfigReader;
 
 public class ShareLinkMakerTest {
   private ShareLinkMaker linkMaker = new ShareLinkMaker();
@@ -12,7 +13,11 @@ public class ShareLinkMakerTest {
   public void testShareLinkMaker() {
     String title = "Fluffy Pancakes with Poached Eggs";
     Account user = new Account("account3", "password");
-    String expectedLink = "http://localhost:8100/share/account3/Fluffy-Pancakes-with-Poached-Eggs";
+    String ipInConfigurationFile = (new ConfigReader()).getRemoteServerIP();
+    String expectedLink =
+        "http://"
+            + ipInConfigurationFile
+            + ":8100/share/account3/Fluffy-Pancakes-with-Poached-Eggs";
     assertEquals(expectedLink, linkMaker.makeLink(title, user));
   }
 }
